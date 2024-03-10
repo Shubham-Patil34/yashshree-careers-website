9860256a12b872a1fac6f44622a75be195e4ba93
Shubham-Patil34 <88846453+Shubham-Patil34@users.noreply.github.com>
2024-03-10 12:39:23 +0000

    Added application form

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import re
 from sqlalchemy import create_engine, text
 import os
 
@@ -19,3 +20,23 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    result=conn.execute(text("SELECT * FROM applications WHERE email = :email"), {"email":data["email"]})
+    rows = result.all()
+    if len(rows) != 0:
+      return False
+    else:
+      query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VAlUES (:job_id, :name, :email, :linkedin_url, :education, :workexp, :resume_url)")
+      result=conn.execute(query, 
+                 {"job_id": job_id, 
+                  "name": data['name'], 
+                  "email": data["email"],
+                  "linkedin_url": data["linkedin_url"],
+                  "education": data["education"],
+                  "workexp": data["workexp"],
+                  "resume_url": data["resume_url"]})
+      conn.commit()
+      return True    
+    
