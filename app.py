d6aa2d4b6a978520d5ca0ad642b8ed99f2c59554
Shubham-Patil34 <88846453+Shubham-Patil34@users.noreply.github.com>
2024-03-11 07:29:40 +0000

    Added ReCaptcha verification

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db, add_application_to_db
-
+import os
+import requests
 app = Flask(__name__)
 
  
@@ -17,22 +18,29 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id,):
   job=load_job_from_db(id)
-  return render_template('jobpage.html', 
-                         job=job)
+  return render_template('jobpage.html',
+                         job=job,
+                         site_key=os.environ['SITE_KEY'])
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
-  data = request.form
-  job=load_job_from_db(id)
- 
-  if add_application_to_db(id, data):
-    return render_template('application_submitted.html',
+  secret_response = request.form['g-recaptcha-response']
+  url = os.environ['VERIFICATION_URL']
+  key = os.environ['SECRET_KEY']
+  verify_response = requests.post(url=f'{url}?secret={key}&response={secret_response}').json()
+  if verify_response['success'] == True:
+    data = request.form
+    job=load_job_from_db(id)
+    if add_application_to_db(id, data):
+      return render_template('application_submitted.html',
                              application=data,
                              job=job)
-  else:
-    return render_template('application_failed.html',    
+    else:
+      return render_template('application_failed.html',    
                            application=data, 
                            job=job)
+  else:
+    return "ReCaptcha Failed..!!"
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
